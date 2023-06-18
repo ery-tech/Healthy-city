6af35665b17b7e81c13ded42790d464244c21a95
Erika <erikaguardo99@gmail.com>
2023-06-18 16:35:01 +0200

    deployed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production : true,
   BASE_URL : 'https://gorest.co.in/public/v2',
-  ACCESS_TOKEN :
-  'Bearer e50ad18a0a4c56f77f2d06b2ae71bd6c05ddfae28a070e3907c68936a1bc221f '
+
 }
